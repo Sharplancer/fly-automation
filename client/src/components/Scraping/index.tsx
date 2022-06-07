632114df0808e7291af9092ff420465bf372cb52
Sharplancer <yevgenyvalentinovich@gmail.com>
2022-06-07 17:39:22 +0100

    update scraping component

--- client/src/components/Scraping/index.tsx
+++ client/src/components/Scraping/index.tsx
@@ -7,10 +7,73 @@ import { RootState } from '../../store/index';
 import { Data } from '../../utils/types';
 
 const ScrapingComponent = () => {
+  const dispatch = useDispatch();
 
+  const status: string = useSelector((state: RootState) => state.scraping.status);
+  const data: Data = useSelector((state: RootState) => state.scraping.data);
+  const urlRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const [url, setUrl] = useState('');
+  const [validated, setValidated] = useState(false);
+  
+  const urlPatternValidation = (URL: string) => {
+    const regex = new RegExp('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?');    
+    return regex.test(URL);
+  };
+
+  const onChangeUrl = (e: any) => {
+    setUrl(e.target.value);
+  }
+
+  const onHandleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const form: any = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      if (urlPatternValidation(url))
+        dispatch<any>(requestScraping(url));
+    }
+//      if(validated === true);
+//        dispatch<any>(requestScraping(url));
+    setValidated(true);
+  }
   return(
     <Container>
-
+      <Form noValidate validated={urlPatternValidation(url) && validated} onSubmit={onHandleSubmit}>
+        <InputGroup className="mb-3 mt-5" hasValidation>
+          <FormControl
+            isInvalid={!urlPatternValidation(url) && validated}
+            placeholder="URL"
+            aria-label="url"
+            aria-describedby="url"
+            onChange={onChangeUrl}
+            ref={urlRef}
+          />
+          <Button variant="primary" type="submit" id="button-addon2" onClick={onHandleSubmit}>
+            Scraping
+          </Button>
+          <Form.Control.Feedback type="invalid">
+            Please type correct URL.
+          </Form.Control.Feedback>
+        </InputGroup>
+      </Form>
+      <div className="d-flex justify-content-center">
+        {
+          status === "pending" && <Spinner animation="border" variant="primary" />
+        }
+        {
+          status === "success" &&
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={data.imgSrc} />
+            <Card.Body>
+              <Card.Title>{data.firstName} {data.lastName}</Card.Title>
+              <Card.Text>
+                Listing: <strong>{data.listingCount}</strong>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        }
+      </div>
     </Container>
   )
 }
